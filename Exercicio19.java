0eb905a0ff29364b61ce5b217c8843e8dd81db6c
Gustavo Boschini <127453464+GustavoBoschi@users.noreply.github.com>
2023-03-16 11:42:27 -0300

    Add files via upload

---
+++ Exercicio19.java
@@ -0,0 +1,20 @@
+package exercicio19;
+
+public class Exercicio19 {
+
+    public static void main(String[] args) {
+
+        int numero;
+        numero = 1;
+
+        while (numero <= 10) {
+            if (numero % 2 == 0) {
+                System.out.println(numero);
+            }
+            numero++;
+
+        }
+
+    }
+
+}
